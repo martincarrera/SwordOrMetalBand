afc05ae64a151c8c65c437dce520a4c774c92c0d
Martin Carrera <mcarrera.92@gmail.com>
2016-04-04 12:21:32 -0300

    Initial Commit

---
+++ SwordOrMetalBand/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwordOrMetalBand
+//
+//  Created by mcarrera on 4/4/16.
+//  Copyright © 2016 Martín Carrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
